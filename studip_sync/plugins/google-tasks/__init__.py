c420dd1124648b0e80da49be02bca43b0ccdfecc
lenke182 <jan@lenke.me>
2020-11-11 01:48:31 +0100

    Code cleanup

--- studip_sync/plugins/google-tasks/__init__.py
+++ studip_sync/plugins/google-tasks/__init__.py
@@ -104,29 +104,11 @@ class Plugin(PluginBase):
     def insert_new_task(self, title, description):
         body = {
             "status": "needsAction",
-            # Status of the task. This is either "needsAction" or "completed".
-            "kind": "tasks#task",  # Type of the resource. This is always "tasks#task".
-            # "updated": "", # Last modification time of the task (as a RFC 3339 timestamp).
-            # "parent": "", # Parent task identifier. This field is omitted if it is a top-level task. This field is read-only. Use the "move" method to move the task under a different parent or to the top level.
-            # "links": [ # Collection of links. This collection is read-only.
-            #  {
-            #    "type": "file", # Type of the link, e.g. "email".
-            #    "link": "file:///home/lenkeja/test.txt", # The URL.
-            #    "description": "ABCDEF", # The description. In HTML speak: Everything between <a> and </a>.
-            #  },
-            # ],
+            "kind": "tasks#task",
             "title": title,  # Title of the task.
             "deleted": False,
-            # Flag indicating whether the task has been deleted. The default if False.
-            # "completed": "A String", # Completion date of the task (as a RFC 3339 timestamp). This field is omitted if the task has not been completed.
-            # "due": "A String", # Due date of the task (as a RFC 3339 timestamp). Optional. The due date only records date information; the time portion of the timestamp is discarded when setting the due date. It isn't possible to read or write the time that a task is due via the API.
-            # "etag": "A String", # ETag of the resource.
             "notes": description,  # Notes describing the task. Optional.
-            # "position": "A String", # String indicating the position of the task among its sibling tasks under the same parent task or at the top level. If this string is greater than another task's corresponding position string according to lexicographical ordering, the task is positioned after the other task under the same parent task (or at the top level). This field is read-only. Use the "move" method to move the task to another position.
             "hidden": False,
-            # Flag indicating whether the task is hidden. This is the case if the task had been marked completed when the task list was last cleared. The default is False. This field is read-only.
-            # "id": "A String", # Task identifier.
-            # "selfLink": "A String", # URL pointing to this task. Used to retrieve, update, or delete this task.
         }
 
         return self.service.tasks().insert(tasklist=self.config.task_list_id, body=body).execute()
