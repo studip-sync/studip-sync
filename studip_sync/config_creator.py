93aeab36e73a98320ab9c26e66ca89fca0700f0c
lenke182 <github-public@lenke.me>
2021-03-28 22:43:33 +0200

    Reformat files to match PEP guidelines

--- studip_sync/config_creator.py
+++ studip_sync/config_creator.py
@@ -6,6 +6,67 @@ from studip_sync.helpers import JSONConfig
 from studip_sync.session import Session
 
 
+def choose_authentication_type():
+    print("Supported authentication methods:")
+    i = 1
+    auth_list = list(AUTHENTICATION_TYPES.items())
+    for auth_key, auth_value in auth_list:
+        print("{}) {}".format(i, auth_value.name()))
+        i += 1
+
+    print()
+
+    try:
+        auth_id = int(input("Choose an authentication method: "))
+    except ValueError:
+        raise ValueError("Please enter a valid number!")
+
+    if auth_id <= 0 or auth_id > len(auth_list):
+        raise ValueError("Please enter a valid number!")
+
+    return auth_list[auth_id - 1]
+
+
+def choose_preset():
+    print("Supported universities:")
+    i = 1
+    for preset in LOGIN_PRESETS:
+        print("{}) {}".format(i, preset.name, preset.base_url))
+        i += 1
+
+    print("{}) Custom server".format(i))
+    print()
+
+    try:
+        preset_id = int(input("Choose a server: "))
+    except ValueError:
+        print("Invalid input! Defaulting to custom server...")
+        return None
+
+    if preset_id == i:
+        return None
+
+    if preset_id <= 0 or preset_id > len(LOGIN_PRESETS):
+        print("Invalid input! Defaulting to custom server...")
+        return None
+
+    return LOGIN_PRESETS[preset_id - 1]
+
+
+def get_url_and_auth_type():
+    selected_preset = choose_preset()
+
+    if selected_preset is not None:
+        return selected_preset.base_url, selected_preset.auth_type, selected_preset.auth_data
+
+    # Otherwise ask for them interactively
+    base_url = input("URL of StudIP: ")
+    auth_key, auth_type = choose_authentication_type()
+    auth_data = auth_type.config_creator_get_auth_data()
+
+    return base_url, auth_key, auth_data
+
+
 class ConfigCreator(object):
     """Create a new config file interactively"""
 
@@ -19,66 +80,8 @@ class ConfigCreator(object):
     def __exit__(self, exc_type, exc_value, traceback):
         self._session.__exit__(exc_type, exc_value, traceback)
 
-    def __choose_preset(self):
-        print("Supported universities:")
-        i = 1
-        for preset in LOGIN_PRESETS:
-            print("{}) {}".format(i,preset.name, preset.base_url))
-            i += 1
-
-        print("{}) Custom server".format(i))
-        print()
-
-        try:
-            preset_id = int(input("Choose a server: "))
-        except:
-            print("Invalid input! Defaulting to custom server...")
-            return None
-
-        if preset_id == i:
-            return None
-
-        if preset_id <= 0 or preset_id > len(LOGIN_PRESETS):
-            print("Invalid input! Defaulting to custom server...")
-            return None
-
-        return LOGIN_PRESETS[preset_id - 1]
-
-    def __choose_authentication_type(self):
-        print("Supported authentication methods:")
-        i = 1
-        auth_list = list(AUTHENTICATION_TYPES.items())
-        for auth_key, auth_value in auth_list:
-            print("{}) {}".format(i, auth_value.name()))
-            i += 1
-
-        print()
-
-        try:
-            auth_id = int(input("Choose an authentication method: "))
-        except:
-            raise ValueError("Please enter a valid number!")
-
-        if auth_id <= 0 or auth_id > len(auth_list):
-            raise ValueError("Please enter a valid number!")
-
-        return auth_list[auth_id - 1]
-
-    def __get_url_and_auth_type(self):
-        selected_preset = self.__choose_preset()
-
-        if selected_preset != None:
-            return selected_preset.base_url, selected_preset.auth_type, selected_preset.auth_data
-
-        # Otherwise ask for them interactively
-        base_url = input("URL of StudIP: ")
-        auth_key, auth_type = self.__choose_authentication_type()
-        auth_data = auth_type.config_creator_get_auth_data()
-
-        return base_url, auth_key, auth_data
-
     def new_config(self):
-        base_url, auth_key, auth_data = self.__get_url_and_auth_type()
+        base_url, auth_key, auth_data = get_url_and_auth_type()
         username = input("Username: ")
         password = getpass.getpass()
 
@@ -92,8 +95,11 @@ class ConfigCreator(object):
         files_destination = input("Sync files to directory (leave empty to disable): ")
         media_destination = input("Sync media to directory (leave empty to disable): ")
 
-        config = {}
-        config["user"] = {"login": username}
+        config = {
+            "user": {
+                "login": username
+            }
+        }
 
         if base_url:
             config["base_url"] = base_url
@@ -122,4 +128,3 @@ class ConfigCreator(object):
         path = get_config_file()
 
         JSONConfig.save_config(path, config)
-
